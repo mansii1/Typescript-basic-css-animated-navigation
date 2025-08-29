60e5703a5423feac9cc56e9dae578e86409efa2a
mansii1 <mansimangukiya0@gmail.com>
2025-08-29 23:21:35 +0530

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import "./style.css";
+
+const toggle = document.getElementById("toggle") as HTMLDivElement;
+const nav = document.getElementById("nav") as HTMLDivElement;
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
